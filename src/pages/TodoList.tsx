08b9ae2f5490d0fc4ef0778b252c4fffb7dd762e
fanfarexo <fanfarexo@gmail.com>
2024-09-28 21:22:25 +0900

    feat: TodoList, TodoItem 컴포넌트 작성

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,5 +1,32 @@
-const TodoList = () => {
-  return <div>TodoList</div>;
+import { Link } from 'react-router-dom';
+import TodoItem from './TodoItem';
+import { CallbacksType, StatesType } from '../AppContainer';
+
+type PropsType = {
+  states: StatesType;
+  callbacks: CallbacksType;
+};
+
+const TodoList = ({ states, callbacks }: PropsType) => {
+  const todoItem = states.todoList.map((item) => {
+    return <TodoItem key={item.id} todoItem={item} callbacks={callbacks} />;
+  });
+  return (
+    <>
+      <div className='row'>
+        <div className='col p-3'>
+          <Link className='btn btn-primary' to='/todos/add'>
+            할 일 추가
+          </Link>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col'>
+          <ul className='list-group'>{todoItem}</ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TodoList;
