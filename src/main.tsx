4a6f1f976c03081320960f728730de214a4601ea
fanfarexo <fanfarexo@gmail.com>
2024-09-28 20:04:53 +0900

    chore: 디렉토리 구조 설정 및 기본 컴포넌트 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './App.tsx';
 import './index.css';
+import AppContainer from './AppContainer';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppContainer />
   </React.StrictMode>,
 );
